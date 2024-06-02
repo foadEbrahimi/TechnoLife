865c686d376f6495f2b3fe19175944dde3f8de7b
Foad <fwad40901@gmail.com>
2024-06-02 20:48:46 +0330

    fix overflow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,30 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper('.mySwiper', {
   // spaceBetween: 10,
   loop: true,
   speed: 1000,
   autoplay: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
-var swiper2 = new Swiper(".mySwiper2", {
+var swiper2 = new Swiper('.mySwiper2', {
   spaceBetween: 100,
   speed: 1000,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
-var swiper = new Swiper(".offer-swiper", {
+var swiper = new Swiper('.offer-swiper', {
   slidesPerView: 2,
   spaceBetween: 10,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
