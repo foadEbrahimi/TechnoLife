e023e6634d71c312fe7430cca71f6c746c3a1ff7
Foad <fwad40901@gmail.com>
2024-06-16 01:04:15 +0330

    fix Slider number one

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,41 @@
 var swiper = new Swiper('.mySwiper', {
   // spaceBetween: 10,
   loop: true,
-  speed: 1000,
+  speed: 1500,
   autoplay: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
 });
 var swiper2 = new Swiper('.mySwiper2', {
   spaceBetween: 100,
-  speed: 1000,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  autoplay: true,
+  speed: 5000,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    768: {
+      slidesPerView: 5,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 40,
+    },
+    1282: {
+      slidesPerView: 7,
+      spaceBetween: 40,
+    },
   },
 });
 var swiper = new Swiper('.offer-swiper', {
